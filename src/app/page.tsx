78baa70f85b7672a39971c1ad0ac0035475354f8
Annie-Dna <your-email@example.com>
2025-05-30 22:59:19 +0500

    Deploying the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Hero from '@/components/Hero/Hero';
 import LogoCloud from '@/components/LogoCloud/LogoCloud';
 import ServiceSection from '@/components/ServiceSection/ServiceSection';
