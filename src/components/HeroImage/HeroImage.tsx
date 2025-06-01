0f2a66c96010a8c825cd343b06d0861e3f2b945f
Annie-Dna <your-email@example.com>
2025-06-02 02:35:02 +0500

    Build errors

--- src/components/HeroImage/HeroImage.tsx
+++ src/components/HeroImage/HeroImage.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useRef, useState } from 'react';
-import { motion, useScroll } from 'framer-motion';
+import { useScroll } from 'framer-motion';
 import Image from 'next/image';
 import NoiseBackground from '../NoiseBackground';
 import gsap from 'gsap';
