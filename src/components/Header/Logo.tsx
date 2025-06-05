4950adebeef73311df061c60de6fb456b1a55333
Annie-Dna <your-email@example.com>
2025-06-06 01:09:31 +0500

    logo updated

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -8,7 +8,7 @@ const Logo = () => {
       className="text-lg sm:text-xl md:text-2xl font-bold tracking-tight text-white hover:opacity-80 transition-opacity"
       style={{ fontFamily: 'Space Grotesk', fontWeight: 700 }}
     >
-      <Image src="/img/7AVENUE-Logo.png" alt="7AVENUE" width={200} height={200} className="brightness-0 invert" />
+      <Image src="/svg/7AVENUE-Logo 1.svg" alt="7AVENUE" width={150} height={22.22} className="brightness-0 invert" />
     </Link>
   );
 };
